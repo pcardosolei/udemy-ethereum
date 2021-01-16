71971eb0593955e7b2c6acd656e805b78718cb28
Paulo Cardoso <pcardoso.lei@gmail.com>
2021-01-16 18:37:49 +0000

    until section 176

--- kickstart/.next/dist/components/Layout.js
+++ kickstart/.next/dist/components/Layout.js
@@ -10,6 +10,10 @@ var _react2 = _interopRequireDefault(_react);
 
 var _semanticUiReact = require('semantic-ui-react');
 
+var _head = require('next/dist/lib/head.js');
+
+var _head2 = _interopRequireDefault(_head);
+
 var _Header = require('./Header');
 
 var _Header2 = _interopRequireDefault(_Header);
@@ -22,13 +26,26 @@ exports.default = function (props) {
     return _react2.default.createElement(_semanticUiReact.Container, {
         __source: {
             fileName: _jsxFileName,
-            lineNumber: 6
+            lineNumber: 8
         }
-    }, _react2.default.createElement(_Header2.default, {
+    }, _react2.default.createElement(_head2.default, {
         __source: {
             fileName: _jsxFileName,
-            lineNumber: 7
+            lineNumber: 9
+        }
+    }, _react2.default.createElement('link', {
+        async: true,
+        rel: 'stylesheet',
+        href: '//cdn.jsdelivr.net/npm/semantic-ui@2.4.1/dist/semantic.min.css',
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 10
+        }
+    })), _react2.default.createElement(_Header2.default, {
+        __source: {
+            fileName: _jsxFileName,
+            lineNumber: 16
         }
     }), props.children);
 };
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHMvTGF5b3V0LmpzIl0sIm5hbWVzIjpbIlJlYWN0IiwiQ29udGFpbmVyIiwiSGVhZGVyIiwicHJvcHMiLCJjaGlsZHJlbiJdLCJtYXBwaW5ncyI6Ijs7Ozs7O0FBQUEsQUFBTzs7OztBQUNQLEFBQVM7O0FBQ1QsQUFBTyxBQUFZLEFBQ25COzs7Ozs7OztrQkFBZSxVQUFBLEFBQUMsT0FBVSxBQUN0QjsyQkFDSSxBQUFDOztzQkFBRDt3QkFBQSxBQUNJO0FBREo7QUFBQSxLQUFBLGtCQUNJLEFBQUM7O3NCQUFEO3dCQURKLEFBQ0ksQUFDQztBQUREO0FBQUEsY0FGUixBQUNJLEFBRVcsQUFHbEI7QUFQRCIsImZpbGUiOiJMYXlvdXQuanMiLCJzb3VyY2VSb290IjoiL1VzZXJzL3BhdWxvY2FyZG9zby9Qcm9qZWN0cy91ZGVteS1ldGhlcmV1bS9raWNrc3RhcnQifQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImNvbXBvbmVudHMvTGF5b3V0LmpzIl0sIm5hbWVzIjpbIlJlYWN0IiwiQ29udGFpbmVyIiwiSGVhZCIsIkhlYWRlciIsInByb3BzIiwiY2hpbGRyZW4iXSwibWFwcGluZ3MiOiI7Ozs7OztBQUFBLEFBQU87Ozs7QUFDUCxBQUFTOztBQUNULEFBQU87Ozs7QUFDUCxBQUFPLEFBQVksQUFFbkI7Ozs7Ozs7O2tCQUFlLFVBQUEsQUFBQyxPQUFVLEFBQ3RCOzJCQUNJLEFBQUM7O3NCQUFEO3dCQUFBLEFBQ0k7QUFESjtBQUFBLEtBQUEsa0JBQ0ksQUFBQzs7c0JBQUQ7d0JBQUEsQUFDQTtBQURBO0FBQUE7ZUFDQSxBQUVJO2FBRkosQUFFUSxBQUNKO2NBSEosQUFHUzs7c0JBSFQ7d0JBRkosQUFDSSxBQUNBLEFBTUE7QUFOQTtBQUNJLHlCQUtKLEFBQUM7O3NCQUFEO3dCQVJKLEFBUUksQUFDQztBQUREO0FBQUEsY0FUUixBQUNJLEFBU1csQUFHbEI7QUFkRCIsImZpbGUiOiJMYXlvdXQuanMiLCJzb3VyY2VSb290IjoiL1VzZXJzL3BhdWxvY2FyZG9zby9Qcm9qZWN0cy91ZGVteS1ldGhlcmV1bS9raWNrc3RhcnQifQ==
